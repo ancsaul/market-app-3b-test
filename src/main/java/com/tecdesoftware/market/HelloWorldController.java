7eda1166e352e391bdda0fc57ecbcbfcb2754d9d
ancsaul <ancona.saul01@gmail.com>
2025-05-14 11:34:50 -0600

    Adding pdn and dev profiles

--- src/main/java/com/tecdesoftware/market/HelloWorldController.java
+++ src/main/java/com/tecdesoftware/market/HelloWorldController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hola")
     public String saludar(){
-        return "Bye World, Jaime😫🔥😼";
+        return "Bye World, Jaaime😫🔥😼";
     }
 }
