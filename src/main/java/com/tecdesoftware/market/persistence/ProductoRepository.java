bf9972bc58acf1a9acf211651f585378dadfd538
ancsaul <ancona.saul01@gmail.com>
2025-07-02 11:25:01 -0600

    ProductoCrudRepository.java added

--- src/main/java/com/tecdesoftware/market/persistence/ProductoRepository.java
+++ src/main/java/com/tecdesoftware/market/persistence/ProductoRepository.java
@@ -1,8 +1,13 @@
 package com.tecdesoftware.market.persistence;
 import java.util.List;
+import java.util.Optional;
+
 import com.tecdesoftware.market.persistence.crud.ProductoCrudRepository;
 import com.tecdesoftware.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
+//le dice  a spring que esta repositorio se conecta con la DB(data base)
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -11,4 +16,27 @@ public class ProductoRepository {
         return (List<Producto>) productoCrudRepository.findAll();
 
     }
+    
+    //obtiene los prodcutos de la categria
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+    
+    public Optional<List<Producto>> getEscasos(int Cantiddad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(Cantiddad, true);
+    }
+    
+    public Optional<Producto> getProductoById(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+    
+    public Producto Save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+    
+    public void delete(Producto producto) {
+        productoCrudRepository.delete(producto);
+    }
+    
+    
 }
--- src/main/java/com/tecdesoftware/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/tecdesoftware/market/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,18 @@ package com.tecdesoftware.market.persistence.crud;
 import com.tecdesoftware.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    //query methods
+    //sql: SELECT*
+    //FROM Categoria
+    //Where Id_Categoria = ?
+    // Order BY Nombre ASC
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(Integer idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, Boolean estado);
 }
